ac93e60403fa8cabd4a52ed6d6ae4cedf16a5628
Jeroen Hoekx <jeroen@hoekx.be>
2021-04-11 13:31:42 +0200

    Use real source in unit tests
    This allows testing to use a more complex configuration.

--- tests/source/test_csv.py
+++ tests/source/test_csv.py
@@ -3,57 +3,59 @@
 # SPDX-FileCopyrightText: 2021 Timeseer.AI
 # SPDX-License-Identifier: Apache-2.0
 
-import pytest
-
 from dateutil.parser import parse as parse_date
 
-from kukur import SeriesSelector, DataType, Dictionary, InterpolationType
-from kukur.source.csv import from_config
+import kukur.config
+
+from kukur import SeriesSelector, DataType, Dictionary, InterpolationType, Source
+from kukur.source import SourceFactory
 
 
-@pytest.fixture
-def make_series():
-    def make_selector(source: str = "fake", name: str = "test-tag-1"):
-        return SeriesSelector(source, name)
+def get_source(source_name: str) -> Source:
+    source = SourceFactory(
+        kukur.config.from_toml("tests/test_data/Kukur.toml")
+    ).get_source(source_name)
+    assert source is not None
+    return source
 
-    return make_selector
+
+def make_series(source: str, name: str = "test-tag-1") -> SeriesSelector:
+    return SeriesSelector(source, name)
 
 
 START_DATE = parse_date("2020-01-01T00:00:00Z")
 END_DATE = parse_date("2020-11-01T00:00:00Z")
 
 
-def test_dir(make_series):
-    source = from_config({"path": "tests/test_data/csv/dir", "format": "dir"})
-    table = source.get_data(make_series(), START_DATE, END_DATE)
+def test_dir():
+    table = get_source("dir").get_data(
+        make_series("dir", "test-tag-1"), START_DATE, END_DATE
+    )
     assert len(table) == 5
     assert table.column_names == ["ts", "value"]
     assert table["ts"][0].as_py() == START_DATE
     assert table["value"][0].as_py() == 1.0
 
 
-def test_row(make_series):
-    source = from_config({"path": "tests/test_data/csv/row.csv", "format": "row"})
-    table = source.get_data(make_series(), START_DATE, END_DATE)
+def test_row():
+    table = get_source("row").get_data(make_series("row"), START_DATE, END_DATE)
     assert len(table) == 5
     assert table.column_names == ["ts", "value"]
     assert table["ts"][0].as_py() == START_DATE
     assert table["value"][0].as_py() == 1.0
 
 
-def test_pivot(make_series):
-    source = from_config({"path": "tests/test_data/csv/pivot.csv", "format": "pivot"})
-    table = source.get_data(make_series(), START_DATE, END_DATE)
+def test_pivot():
+    table = get_source("pivot").get_data(make_series("pivot"), START_DATE, END_DATE)
     assert len(table) == 7
     assert table.column_names == ["ts", "value"]
     assert table["ts"][0].as_py() == START_DATE
     assert table["value"][0].as_py() == 1.0
 
 
-def test_row_metadata(make_series):
-    source = from_config({"metadata": "tests/test_data/csv/row-metadata.csv"})
-    series = make_series()
-    metadata = source.get_metadata(series)
+def test_row_metadata():
+    series = make_series("row")
+    metadata = get_source("row").get_metadata(series)
     assert metadata.series == series
     assert isinstance(metadata.description, str)
     assert isinstance(metadata.unit, str)
@@ -63,29 +65,16 @@ def test_row_metadata(make_series):
 
 
 def test_row_metadata_dictionary():
-    source = from_config(
-        {
-            "metadata": "tests/test_data/csv/row-metadata.csv",
-            "dictionary_dir": "tests/test_data/csv/dictionary",
-        }
-    )
-    metadata = source.get_metadata(SeriesSelector("fake", "test-tag-6"))
-    assert metadata.series == SeriesSelector("fake", "test-tag-6")
+    metadata = get_source("row").get_metadata(SeriesSelector("row", "test-tag-6"))
+    assert metadata.series == SeriesSelector("row", "test-tag-6")
     assert metadata.data_type == DataType.DICTIONARY
     assert metadata.dictionary_name == "Active"
     assert isinstance(metadata.dictionary, Dictionary)
 
 
-def test_metadata_mapping(make_series):
-    source = from_config(
-        {
-            "metadata": "tests/test_data/csv/mapping-metadata.csv",
-            "metadata_mapping": "tests/test_data/csv/mapping-mapping.csv",
-            "metadata_value_mapping": "tests/test_data/csv/mapping-value-mapping.csv",
-        }
-    )
-    metadata = source.get_metadata(make_series())
-    assert metadata.series == SeriesSelector("fake", "test-tag-1")
+def test_metadata_mapping():
+    metadata = get_source("mapping").get_metadata(make_series("mapping"))
+    assert metadata.series == SeriesSelector("mapping", "test-tag-1")
     assert metadata.unit == "kg"
     assert metadata.limit_low == 1
     assert metadata.interpolation_type == InterpolationType.LINEAR
--- tests/source/test_feather.py
+++ tests/source/test_feather.py
@@ -5,69 +5,72 @@
 
 from dateutil.parser import parse as parse_date
 
-import pytest
+import kukur.config
 
-from kukur import SeriesSelector
-from kukur.source.feather import from_config
+from kukur import SeriesSelector, Source
+from kukur.source import SourceFactory
 
 
 START_DATE = parse_date("2020-01-01T00:00:00Z")
 END_DATE = parse_date("2020-11-01T00:00:00Z")
 
 
-@pytest.fixture
-def make_series():
-    def make_selector(source: str = "fake", name: str = "test-tag-1"):
-        return SeriesSelector(source, name)
+def get_source(source_name: str) -> Source:
+    source = SourceFactory(
+        kukur.config.from_toml("tests/test_data/Kukur.toml")
+    ).get_source(source_name)
+    assert source is not None
+    return source
 
-    return make_selector
+
+def make_series(source: str, name: str = "test-tag-1") -> SeriesSelector:
+    return SeriesSelector(source, name)
 
 
-def test_dir(make_series):
-    source = from_config({"path": "tests/test_data/feather/dir", "format": "dir"})
-    table = source.get_data(make_series(), START_DATE, END_DATE)
+def test_dir():
+    table = get_source("dir-feather").get_data(
+        make_series("dir-feather"), START_DATE, END_DATE
+    )
     assert len(table) == 5
     assert table.column_names == ["ts", "value"]
     assert table["ts"][0].as_py() == START_DATE
     assert table["value"][0].as_py() == 1.0
 
 
-def test_dir_string(make_series):
-    source = from_config({"path": "tests/test_data/feather/dir", "format": "dir"})
-    table = source.get_data(make_series(name="test-tag-5"), START_DATE, END_DATE)
+def test_dir_string():
+    table = get_source("dir-feather").get_data(
+        make_series("dir-feather", "test-tag-5"), START_DATE, END_DATE
+    )
     assert len(table) == 7
     assert table.column_names == ["ts", "value"]
     assert table["ts"][0].as_py() == START_DATE
     assert table["value"][0].as_py() == "A"
 
 
-def test_row(make_series):
-    source = from_config(
-        {"path": "tests/test_data/feather/row.feather", "format": "row"}
+def test_row():
+    table = get_source("row-feather").get_data(
+        make_series("row-feather"), START_DATE, END_DATE
     )
-    table = source.get_data(make_series(), START_DATE, END_DATE)
     assert len(table) == 5
     assert table.column_names == ["ts", "value"]
     assert table["ts"][0].as_py() == START_DATE
     assert table["value"][0].as_py() == 1.0
 
 
-def test_pivot(make_series):
-    source = from_config(
-        {"path": "tests/test_data/feather/pivot.feather", "format": "pivot"}
+def test_pivot():
+    table = get_source("pivot-feather").get_data(
+        make_series("pivot-feather"), START_DATE, END_DATE
     )
-    table = source.get_data(make_series(), START_DATE, END_DATE)
     assert len(table) == 7
     assert table.column_names == ["ts", "value"]
     assert table["ts"][0].as_py() == START_DATE
     assert table["value"][0].as_py() == 1.0
 
 
-def test_pivot_string(make_series):
-    source = from_config(
-        {"path": "tests/test_data/feather/pivot.feather", "format": "pivot"}
+def test_pivot_string():
+    table = get_source("pivot-feather").get_data(
+        make_series("pivot-feather", "test-tag-5"), START_DATE, END_DATE
     )
-    table = source.get_data(make_series(name="test-tag-5"), START_DATE, END_DATE)
     assert len(table) == 7
     assert table.column_names == ["ts", "value"]
     assert table["ts"][0].as_py() == START_DATE
