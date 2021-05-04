87f21fb3021748dd3e99b96c3fc4eea6cd060cce
Wouter <wouter@timeseer.ai>
2021-05-04 16:38:35 +0200

    Ensure the source protocol is given to the KukurFlightServer as initialize value so that other implementations are not broken. Therefore moving list sources to a separate function within the kukur application.

--- kukur/cli.py
+++ kukur/cli.py
@@ -125,7 +125,7 @@ def _serve(kukur_app: Kukur, server_config):
     server.register_action_handler("search", service.search)
     server.register_action_handler("get_metadata", service.get_metadata)
     server.register_get_handler("get_data", service.get_data)
-    server.register_action_handler("list_sources", service.list_sources)
+    server.register_action_handler("list_sources", kukur_app.list_sources)
     server.serve()
 
 
