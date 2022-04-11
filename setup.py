7810476e64069812602ea1b3eb9ac17d2a75300c
Jeroen Hoekx <jeroen@hoekx.be>
2022-04-11 16:30:49 +0200

    Document CrateDB connection

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
       ],
     extras_require={
         "adodb": ["pywin32"],
-        "crate": ["crate"],
+        "cratedb": ["crate"],
         "delta": ["deltalake"],
         "influxdb": ["influxdb"],
         "odbc": ["pyodbc"],
