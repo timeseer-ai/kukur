ec518bdce24681d4ccbd8350ddc15ca4bfc9b829
Lucas Manika Koeb <lkoeb186@gmail.com>
2022-09-22 14:56:53 -0300

    Use field as values column and make timestamps_column configurable

--- kukur/source/data_explorer/data_explorer.py
+++ kukur/source/data_explorer/data_explorer.py
@@ -3,10 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import os
-
 from datetime import datetime
-from typing import Any, Dict, Generator, List, Optional, Tuple
+from typing import Any, Dict, Generator, Optional
 
 import pyarrow as pa
 
@@ -44,7 +42,8 @@ def from_config(config: Dict[str, Any]):
     connection_string = config["connection_string"]
     database = config["database"]
     table = config["table"]
-    return DataExplorerSource(connection_string, database, table)
+    timestamp_column = config.get("timestamp_column", "ts")
+    return DataExplorerSource(connection_string, database, table, timestamp_column)
 
 
 class DataExplorerSource:
@@ -53,20 +52,25 @@ class DataExplorerSource:
     __database: str
     __table: str
     __connection_string: str
-    __azure_credential: DefaultAzureCredential
+
+    if HAS_AZURE_IDENTITY:
+        __azure_credential: DefaultAzureCredential
 
     if HAS_KUSTO:
         __client: KustoClient
 
-    def __init__(self, connection_string: str, database: str, table: str):
+    def __init__(
+        self, connection_string: str, database: str, table: str, timestamp_column: str
+    ):
         if not HAS_AZURE_IDENTITY:
             raise MissingModuleException("azure-identity")
 
         if not HAS_KUSTO:
             raise MissingModuleException("data_explorer", "azure-kusto-data")
 
-        self.__database = database
-        self.__table = table
+        self.__database = _escape(database)
+        self.__table = _escape(table)
+        self.__timestamp_column = _escape(timestamp_column)
         self.__connection_string = connection_string
         self.__azure_credential = DefaultAzureCredential()
 
@@ -90,8 +94,8 @@ class DataExplorerSource:
         """Return data for the given time series in the given time period."""
 
         query = f"""['{self.__table}']
-            | where ts >= todatetime('{start_date}')
-            | where ts <= todatetime('{end_date}')
+            | where {self.__timestamp_column} >= todatetime('{start_date}')
+            | where {self.__timestamp_column} <= todatetime('{end_date}')
         """
 
         for (tag_key, tag_value) in selector.tags.items():
@@ -103,33 +107,20 @@ class DataExplorerSource:
 
         if result is not None and len(result.primary_results) > 0:
             for row in result.primary_results[0]:
-                timestamps.append(row["ts"])
-                values.append(row["value"])
+                timestamps.append(row[self.__timestamp_column])
+                values.append(row[selector.field])
 
         return pa.Table.from_pydict({"ts": timestamps, "value": values})
 
     def get_source_structure(self, _: SeriesSelector) -> Optional[SourceStructure]:
         """Return the available tag keys, tag value and tag fields."""
-        query_tag_keys = ".show database schema"
-        result = self.__client.execute(self.__database, query_tag_keys)
-        tag_keys = [
-            row["ColumnName"]
-            for row in result.primary_results[0]
-            if row["ColumnName"] is not None
-            and row["ColumnName"] != "ts"
-            and row["ColumnName"] != "value"
-        ]
-
-        tag_values: List[dict] = []
-        for key in tag_keys:
-            query_tag_values = f"['{self.__table}'] | project {key} | distinct {key}"
-            result = self.__client.execute(self.__database, query_tag_values)
-            tag_values.append({key: [row[key] for row in result.primary_results[0]]})
-        return SourceStructure([], tag_keys, tag_values)
+        raise NotImplementedError()
 
     def _get_auth_token(self) -> str:
         """Uses azure default authentication to get an auth token for the configured cluster."""
-        return self.__azure_credential.get_token(self.__connection_string + '//.default')[0]
+        return self.__azure_credential.get_token(
+            self.__connection_string + "//.default"
+        )[0]
 
 
 def _escape(context: Optional[str]) -> str:
