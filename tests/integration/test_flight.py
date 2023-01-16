095aa9b341044dd86ee1a030ca38b73d29243bed
OsvaldoBecker <osvaldo@timeseer.ai>
2023-01-16 13:44:00 +0100

    Fix sources length test

--- tests/integration/test_flight.py
+++ tests/integration/test_flight.py
@@ -110,7 +110,7 @@ def test_plot_data_fallback(client: Client):
 
 def test_sources(client: Client):
     data = client.list_sources()
-    assert len(data) == 83
+    assert len(data) == 78
 
     assert "sql" in data
     assert "row" in data
