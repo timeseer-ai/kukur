1c8bec1c13abefd589c7c90c37148a632539bae5
WouterWouters <wouter@timeseer.ai>
2024-11-13 11:32:10 +0100

    Use Query DSL for querying to Elasticsearch

--- kukur/source/elasticsearch/elasticsearch.py
+++ kukur/source/elasticsearch/elasticsearch.py
@@ -58,11 +58,12 @@ def from_config(
     configuration = ElasticsearchSourceConfiguration(
         host, port, username, password, api_key
     )
+    index = config["index"]
+    list_index = config.get("list_index", index)
 
     options = ElasticsearchSourceOptions(
-        config["list_query"],
-        config["metadata_query"],
-        config["data_query"],
+        index,
+        list_index,
         config.get("tag_columns", ["series name"]),
         config.get("field_columns", ["value"]),
         config.get("metadata_columns", []),
@@ -91,9 +92,8 @@ class ElasticsearchSourceConfiguration:
 class ElasticsearchSourceOptions:
     """Options for Elasticsearch sources."""
 
-    list_query: str
-    metadata_query: str
-    data_query: str
+    index: str
+    list_index: str
     tag_columns: List[str]
     field_columns: List[str]
     metadata_columns: List[str]
@@ -119,9 +119,10 @@ class ElasticsearchSource:
 
     def search(self, selector: SeriesSearch) -> Generator[Metadata, None, None]:
         """Search for series matching the given selector."""
-        table = self._search_esql(selector)
+        table = self._search_query_dsl({})
 
-        for row in table.to_pylist():
+        for one_row in table:
+            row = one_row["_source"]
             tags = {
                 self.__metadata_mapper.from_source(tag_name): row[tag_name]
                 for tag_name in self.__options.tag_columns
@@ -147,33 +148,9 @@ class ElasticsearchSource:
                         )
                     yield metadata
 
-    def _search_esql(self, selector: SeriesSearch) -> pa.Table:
-
-        query = self.__options.list_query
-        if selector.tags is not None and len(selector.tags) > 0:
-            for tag_key, tag_value in selector.tags.items():
-                query += f' | where {_escape(self.__metadata_mapper.from_kukur(tag_key))} == "{_escape(tag_value)}"'
-
-        if selector.field is not None:
-            column_name = self._get_field_column_name()
-            query += f' | where {column_name} == "{_escape(selector.field)}"'
-
-        content = self._send_query(query)
-
-        columns: Dict[str, List] = {}
-        for index, column in enumerate(content["columns"]):
-            if (
-                column["name"] in self.__options.tag_columns
-                or column["name"] in self.__options.metadata_columns
-                or column["name"] in self.__options.field_columns
-                or column["name"] == self.__options.metadata_field_column
-            ):
-                if column["name"] in columns:
-                    columns[column["name"]].extend(content["values"][index])
-                else:
-                    columns[column["name"]] = content["values"][index]
-
-        return pa.Table.from_pydict(columns)
+    def _search_query_dsl(self, query: dict) -> List:
+        content = self._send_search_query(query, self.__options.list_index)
+        return content["hits"]["hits"]
 
     def _get_field_column_name(self):
         column_name = "value"
@@ -186,13 +163,30 @@ class ElasticsearchSource:
 
     def get_metadata(self, selector: SeriesSelector) -> Metadata:
         """Read metadata, taking any configured metadata mapping into account."""
-        table = self._metadata_esql(selector)
+        fields = []
+        if self.__options.metadata_field_column is not None:
+            fields = [{"term": {self.__options.metadata_field_column: selector.field}}]
+        query = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "term": {
+                                self.__metadata_mapper.from_kukur(tag_name): tag_value
+                            }
+                        }
+                        for tag_name, tag_value in selector.tags.items()
+                    ]
+                    + fields
+                }
+            }
+        }
+        rows = self._search_query_dsl(query)
 
         metadata = Metadata(selector)
-        rows = table.to_pylist()
         if len(rows) == 0:
             return metadata
-        row = rows[0]
+        row = rows[0]["_source"]
 
         field_names = [field for field, _ in metadata.iter_names()]
 
@@ -211,61 +205,47 @@ class ElasticsearchSource:
                     pass
         return metadata
 
-    def _metadata_esql(self, selector: SeriesSelector) -> pa.Table:
-        query = self.__options.metadata_query
-        column_name = self._get_field_column_name()
-        query += f' | where {column_name} == "{_escape(selector.field)}"'
-        if len(selector.tags) > 0:
-            for tag_key, tag_value in selector.tags.items():
-                query += f' | where {_escape(self.__metadata_mapper.from_kukur(tag_key))} == "{_escape(tag_value)}"'
-
-        content = self._send_query(query)
-        columns = {}
-        for index, column in enumerate(content["columns"]):
-            if (
-                column["name"] in self.__options.tag_columns
-                or column["name"] in self.__options.metadata_columns
-                or column["name"] in self.__options.field_columns
-                or column["name"] == self.__options.metadata_field_column
-            ):
-                columns[column["name"]] = content["values"][index]
-
-        return pa.Table.from_pydict(columns)
-
     def get_data(
         self, selector: SeriesSelector, start_date: datetime, end_date: datetime
     ) -> pa.Table:
         """Return data for the given time series in the given time period."""
+        query = {
+            "bool": {
+                "must": [
+                    {"term": {self.__metadata_mapper.from_kukur(tag_name): tag_value}}
+                    for tag_name, tag_value in selector.tags.items()
+                ],
+                "filter": {
+                    "range": {
+                        self.__options.timestamp_column: {
+                            "gte": start_date.isoformat(),
+                            "lte": end_date.isoformat(),
+                        }
+                    },
+                },
+            }
+        }
+        data_query = {
+            "query": query,
+            "fields": [self.__options.timestamp_column, selector.field],
+            "sort": [{self.__options.timestamp_column: "asc"}],
+            "_source": False,
+        }
+
         timestamps = []
         values = []
+        search_after = None
         while True:
-            query = self.__options.data_query
-            split_query = self.__options.data_query.split("|", 1)
-            query = split_query[0]
-            params = [start_date.isoformat(), end_date.isoformat()]
-            if len(selector.tags) > 0:
-                for tag_key, tag_value in selector.tags.items():
-                    query += f' | where {_escape(self.__metadata_mapper.from_kukur(tag_key))} == "{_escape(tag_value)}"'
-
-            if len(split_query) > 1:
-                query += f" | {split_query[1]}"
-
-            query += f" | keep {self.__options.timestamp_column}, {selector.field}"
-            content = self._send_query(query, params)
-
-            for index, column in enumerate(content["columns"]):
-                if column["name"] == self.__options.timestamp_column:
-                    timestamps.extend(content["values"][index])
-                if column["name"] == selector.field:
-                    values.extend(content["values"][index])
-
-            if len(content["values"][0]) != self.__options.max_number_of_rows:
+            if search_after is not None:
+                query["search_after"] = search_after
+            data = self._send_search_query(data_query, self.__options.index)
+            for row in data["hits"]["hits"]:
+                fields = row["fields"]
+                timestamps.extend(fields[self.__options.timestamp_column])
+                values.extend(fields[selector.field])
+                search_after = row["sort"]
+            if len(timestamps) == data["hits"]["total"]["value"]:
                 break
-
-            start_date = timestamps[-1]
-            while timestamps[-1] == start_date:
-                timestamps.pop()
-                values.pop()
 
         return pa.Table.from_pydict({"ts": timestamps, "value": values})
 
@@ -295,16 +275,20 @@ class ElasticsearchSource:
         response.raise_for_status()
         return json.loads(response.content)
 
+    def _send_search_query(self, query: dict, index: str) -> Dict:
 
-def _escape(context: Optional[str]) -> str:
-    if context is None:
-        context = "value"
-    if "'" in context:
-        context = context.replace("'", "")
-    if '"' in context:
-        context = context.replace('"', "")
-    if "|" in context:
-        context = context.replace("|", "")
-    if ";" in context:
-        context = context.replace(";", "")
-    return context
+        headers = {"Content-Type": "application/json"}
+        auth = None
+        if self.__configuration.api_key is not None:
+            headers["Authorization"] = f"ApiKey {self.__configuration.api_key}"
+        else:
+            auth = (self.__configuration.username, self.__configuration.password)
+        response = requests.post(
+            f"{self.__configuration.host}:{self.__configuration.port}/{index}/_search",
+            auth=auth,
+            headers=headers,
+            json=query,
+            timeout=60,
+        )
+        response.raise_for_status()
+        return json.loads(response.content)
--- tests/integration/test_elasticsearch.py
+++ tests/integration/test_elasticsearch.py
@@ -40,41 +40,6 @@ def test_search(client: Client):
     assert series.get_field(fields.LimitHighFunctional) == 9
 
 
-def test_search_with_tags(client: Client):
-    many_series = list(
-        client.search(
-            SeriesSearch(
-                "noaa-es",
-                {"series name": "h2o", "location": "coyote_creek"},
-            )
-        )
-    )
-    assert len(many_series) == 2
-    series = [series for series in many_series if series.series.field == "water_level"][
-        0
-    ]
-    assert isinstance(series, Metadata)
-    assert series.get_field(fields.LimitLowFunctional) == 6
-    assert series.get_field(fields.LimitHighFunctional) == 9
-
-
-def test_search_with_fields(client: Client):
-    many_series = list(
-        client.search(
-            SeriesSearch(
-                "noaa-es",
-                {"series name": "h2o", "location": "coyote_creek"},
-                "water_level",
-            )
-        )
-    )
-    assert len(many_series) == 1
-    series = many_series[0]
-    assert isinstance(series, Metadata)
-    assert series.get_field(fields.LimitLowFunctional) == 6
-    assert series.get_field(fields.LimitHighFunctional) == 9
-
-
 def test_metadata(client: Client):
     tags = {
         "series name": "h2o",
@@ -102,6 +67,6 @@ def test_data(client: Client):
     )
     assert len(data) == 241
     assert data["ts"][0].as_py() == start_date
-    assert data["value"][0].as_py() == 9.982634544372559
+    assert data["value"][0].as_py() == 9.982635
     assert data["ts"][240].as_py() == end_date
-    assert data["value"][240].as_py() == 6.944540977478027
+    assert data["value"][240].as_py() == 6.944541
