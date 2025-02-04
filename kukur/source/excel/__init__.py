e2c9100f489ab4034d22e83e4e5270e91132686f
hammsvietro <hammsvietro@gmail.com>
2025-02-04 13:17:14 -0300

    Drop usage of pandas

--- kukur/source/excel/__init__.py
+++ kukur/source/excel/__init__.py
@@ -11,81 +11,62 @@ from kukur.inspect import DataOptions
 
 try:
 
-    import pandas as pd
-
-    HAS_PANDAS = True
-except ImportError:
-    HAS_PANDAS = False
-
-try:
-
     import openpyxl
 
-    HAS_PYOPENXL = True
+    HAS_OPENPYXL = True
 
 except ImportError:
-    HAS_PYOPENXL = False
+    HAS_OPENPYXL = False
 
 
 from kukur.exceptions import MissingModuleException
 
 
 def parse_excel(readable, sheet_name: str, options: Optional[DataOptions]) -> pa.Table:
-    """Parse a excel file to a PyArrow Table."""
-    if not HAS_PANDAS:
-        raise MissingModuleException("pandas", "excel")
-    if not HAS_PYOPENXL:
+    """Parse an Excel file to a PyArrow Table."""
+    if not HAS_OPENPYXL:
         raise MissingModuleException("openpyxl", "excel")
 
     workbook = openpyxl.load_workbook(readable, data_only=True)
-    worksheet = workbook[sheet_name or workbook.sheetnames[0]]
+    worksheet = workbook[sheet_name]
 
     data = list(worksheet.values)
+    workbook.close()
+
+    if len(data) == 0:
+        return _to_pyarrow([], [])
     if options is not None and options.excel_header_row:
         headers = data[0]
         rows = data[1:]
-        excel_df = pd.DataFrame(rows, columns=headers)
     else:
-        excel_df = pd.DataFrame(data)
-    return _to_pyarrow(excel_df)
+        headers = [str(i) for i in range(len(data[0]))] if len(data) > 0 else []
+        rows = data
 
-
-def parse_table(readable, sheet_name: str, table_name: str) -> pa.Table:
-    """Parse a table from a excel file to a PyArrow Table."""
-    wb = openpyxl.load_workbook(readable)
-    ws = wb[sheet_name or wb.sheetnames[0]]
-    table = ws.tables[table_name]
-
-    table_range = table.ref
-    rows = openpyxl.utils.cell.range_boundaries(table_range)
-
-    data = []
-    for row in ws.iter_rows(
-        rows,
-        values_only=True,
-    ):
-        data.append(row)
-
-    excel_df = pd.DataFrame(data[1:], columns=data[0])
-    return _to_pyarrow(excel_df)
+    return _to_pyarrow([str(header) for header in headers], rows)
 
 
 def list_sheets(readable) -> List[str]:
     """List the sheets in the Excel file."""
-    if not HAS_PYOPENXL:
+    if not HAS_OPENPYXL:
         raise MissingModuleException("openpyxl", "excel")
 
     workbook = openpyxl.load_workbook(readable)
     return workbook.sheetnames
 
 
-def _to_pyarrow(excel_df: pd.DataFrame) -> pa.Table:
-    """Convert a pandas DataFrame to a PyArrow Table."""
-    for col in excel_df.columns:
-        unique_types = set(map(type, excel_df[col].dropna()))
-        if len(unique_types) > 1:
-            excel_df[col] = excel_df[col].astype(str)
+def _to_pyarrow(headers: List[str], rows: List[List]) -> pa.Table:
+    """Convert raw excel to a PyArrow table."""
+    arrays = []
+    columns = list(zip(*rows))
+    for column in columns:
+        try:
+            arrays.append(pa.array(column))
+        except pa.lib.ArrowException:
+            arrays.append(
+                pa.array(
+                    [str(value) if value is not None else None for value in column],
+                    type=pa.string(),
+                )
+            )
 
-    excel_df = excel_df.convert_dtypes(dtype_backend="pyarrow")
-
-    return pa.Table.from_pandas(excel_df, preserve_index=False)
+    return pa.table(arrays, names=headers)
