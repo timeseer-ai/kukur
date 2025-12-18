b053bfb914a26a9f3e1af90ec78a0b3ffbb1914a
Lucas Manika Koeb <lucas@timeseer.ai>
2025-12-18 17:09:21 -0300

    Limit results on adx queries to the specified limit of rows
    Support paginated queries to handle larger data sets.

--- tests/source/test_azure_data_explorer.py
+++ tests/source/test_azure_data_explorer.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2022 Timeseer.AI
 # SPDX-License-Identifier: Apache-2.0
 
+import re
 from datetime import datetime, timedelta, timezone
 from random import random
 from typing import Any
@@ -119,8 +120,10 @@ def test_source_structure(_kusto_client) -> None:
     assert location_values == {"Antwerp", "Curitiba"}
 
 
-get_data_response = MockKustoResponse(
-    [
+def get_data_response(_database, query):
+    limit = _get_limit_from_query(query)
+    offset = _get_offset_from_query(query)
+    response_data = [
         {
             "ts": datetime.now(tz=timezone.utc) - timedelta(minutes=10),
             "pressure": random() * 100,
@@ -137,11 +140,56 @@ get_data_response = MockKustoResponse(
             "ts": datetime.now(tz=timezone.utc) - timedelta(minutes=7),
             "pressure": random() * 100,
         },
+        {
+            "ts": datetime.now(tz=timezone.utc) - timedelta(minutes=6),
+            "pressure": random() * 100,
+        },
+        {
+            "ts": datetime.now(tz=timezone.utc) - timedelta(minutes=5),
+            "pressure": random() * 100,
+        },
+        {
+            "ts": datetime.now(tz=timezone.utc) - timedelta(minutes=4),
+            "pressure": random() * 100,
+        },
+        {
+            "ts": datetime.now(tz=timezone.utc) - timedelta(minutes=3),
+            "pressure": random() * 100,
+        },
+        {
+            "ts": datetime.now(tz=timezone.utc) - timedelta(minutes=2),
+            "pressure": random() * 100,
+        },
+        {
+            "ts": datetime.now(tz=timezone.utc) - timedelta(minutes=1),
+            "pressure": random() * 100,
+        },
     ]
-)
+    if offset is not None:
+        response_data = response_data[offset:]
+    if limit is not None:
+        response_data = response_data[:limit]
+
+    return MockKustoResponse(response_data)
 
 
-@patch("azure.kusto.data.KustoClient.execute", return_value=get_data_response)
+def _get_limit_from_query(query) -> int | None:
+    limit = None
+    match = re.search(r"\btake (\d+)", query)
+    if match is not None:
+        limit = int(match.group(1))
+    return limit
+
+
+def _get_offset_from_query(query) -> int | None:
+    offset = None
+    match = re.search(r"\brow_number\(\) > (\d+)", query)
+    if match is not None:
+        offset = int(match.group(1))
+    return offset
+
+
+@patch("azure.kusto.data.KustoClient.execute", side_effect=get_data_response)
 def test_get_data(kusto_client) -> None:
     source = from_config(
         {
@@ -166,7 +214,36 @@ def test_get_data(kusto_client) -> None:
     assert "['plant']=='Plant02'" in args[1]
     assert f"['ts'] >= todatetime('{initial_date}')" in args[1]
     assert f"['ts'] <= todatetime('{final_date}')" in args[1]
-    assert len(data) == 4
+    assert len(data) == 10
+
+
+@patch("kukur.source.azure_data_explorer.azure_data_explorer.MAX_ITEMS_PER_CALL", 3)
+@patch("azure.kusto.data.KustoClient.execute", side_effect=get_data_response)
+def test_get_data_multiple_calls(kusto_client) -> None:
+    source = from_config(
+        {
+            "connection_string": "https://test_cluster.net",
+            "database": "telemetry",
+            "table": "telemetry-data",
+            "tag_columns": ["deviceId", "plant", "sensorModel", "location"],
+            "ignored_columns": ["pressureUnit", "temperatureUnit"],
+        },
+        None,
+        None,
+    )
+    selector = SeriesSelector(
+        "my_source", {"location": "Curitiba", "plant": "Plant02"}, "pressure"
+    )
+    initial_date = datetime.now(tz=timezone.utc) - timedelta(minutes=20)
+    final_date = datetime.now(tz=timezone.utc)
+    data = source.get_data(selector, initial_date, final_date)
+    args = kusto_client.call_args.args
+    assert args[0] == "telemetry"
+    assert "['location']=='Curitiba'" in args[1]
+    assert "['plant']=='Plant02'" in args[1]
+    assert f"['ts'] >= todatetime('{initial_date}')" in args[1]
+    assert f"['ts'] <= todatetime('{final_date}')" in args[1]
+    assert len(data) == 10
 
 
 @patch("azure.kusto.data.KustoClient.execute", side_effect=source_structure_queries)
