18f7202fc31e508ad0631285a83bafa3abfb7567
Jeroen Hoekx <jeroen@timeseer.ai>
2026-07-31 09:43:51 +0200

    One source was added to the test data

--- tests/integration/test_flight.py
+++ tests/integration/test_flight.py
@@ -116,7 +116,7 @@ def test_plot_data_fallback(client: Client):
 
 def test_sources(client: Client):
     data = client.list_sources()
-    assert len(data) == 144
+    assert len(data) == 145
 
     assert "sql" in data
     assert "row" in data
